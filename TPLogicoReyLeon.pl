4a4e4d2c5a1e1a7e16f3dd6e9acfa76f1e21ad3a
Icethunder76 <joaquin_ponte_v@hotmail.com>
2023-08-06 01:11:44 -0300

    Se realizo el punto 2 y se copio el enunciado del 3

--- TPLogicoReyLeon.pl
+++ TPLogicoReyLeon.pl
@@ -3,31 +3,47 @@
 %cucarachas(nombre,tamanio,peso).
 %hormigas(nombre).
 
-%comio(Personaje, Bicho) 
-comio(pumba, vaquitaSanAntonio(gervasia,3)). 
-comio(pumba, hormiga(federica)). 
-comio(pumba, hormiga(tuNoEresLaReina)). 
-comio(pumba, cucaracha(ginger,15,6)). 
-comio(pumba, cucaracha(erikElRojo,25,70)). 
+%comio(Personaje, Bicho)
+comio(pumba, vaquitaSanAntonio(gervasia,3)).
+comio(pumba, hormiga(federica)).
+comio(pumba, hormiga(tuNoEresLaReina)).
+comio(pumba, cucaracha(ginger,15,6)).
+comio(pumba, cucaracha(erikElRojo,25,70)).
 
-comio(timon, vaquitaSanAntonio(romualda,4)). 
-comio(timon, cucaracha(gimeno,12,8)). 
-comio(timon, cucaracha(cucurucha,12,5)). 
+comio(timon, vaquitaSanAntonio(romualda,4)).
+comio(timon, cucaracha(gimeno,12,8)).
+comio(timon, cucaracha(cucurucha,12,5)).
 
-comio(simba, vaquitaSanAntonio(remeditos,4)). 
-comio(simba, hormiga(schwartzenegger)). 
-comio(simba, hormiga(niato)). 
-comio(simba, hormiga(lula)). 
+comio(simba, vaquitaSanAntonio(remeditos,4)).
+comio(simba, hormiga(schwartzenegger)).
+comio(simba, hormiga(niato)).
+comio(simba, hormiga(lula)).
 
-pesoHormiga(2). 
+% Punto2)
+comio(shenzi,hormiga(conCaraDeSimba)).
+
+pesoHormiga(2).
 
 %peso(Personaje, Peso) 
-peso(pumba, 100). 
-peso(timon, 50). 
-peso(simba, 200). 
+peso(pumba, 100).
+peso(timon, 50).
+peso(simba, 200).
+
+% Punto2)
+peso(scar, 300).
+peso(shenzi, 400).
+peso(banzai, 500).
+
+persigue(scar, timon). 
+persigue(scar, pumba).
+persigue(shenzi, simba).
+persigue(shenzi, scar).
+persigue(banzai, timon).
+
 
 
 % 1)a) Qué cucaracha es jugosita: ó sea, hay otra con su mismo tamaño 
+
 %      pero ella es más gordita. 
 %?-jugosita(cucaracha(gimeno,12,8)). 
 %Yes 
@@ -39,6 +55,7 @@ jugosita(cucaracha(_,Tamanio,Peso1)) :-
     max_member(Peso1, ListaPesos).
 
 % 1)b) Si un personaje es hormigofílico... (Comió al menos dos hormigas).
+
 %?-hormigofilico(X). 
 %X = pumba; 
 %X = simba. 
@@ -48,9 +65,10 @@ hormigofilico(Personaje) :-
     findall(Nombre,comio(Personaje,hormiga(Nombre)),ComioHormiga),
     length(ComioHormiga,Cantidad),
     Cantidad >= 2.
-personajes(Personaje):- peso(Personaje,_).
+personajes(Personaje):- comio(Personaje,vaquitaSanAntonio(_,_)).
 
 % 1)c) Si un personaje es cucarachofóbico (no comió cucarachas). 
+
 %?-cucarachofobico(X). 
 %X = simba
 
@@ -63,6 +81,7 @@ cucarachofobico(Personaje) :-
 % 1)d) Conocer al conjunto de los picarones. 
 %      Un personaje es picarón si comió una cucaracha jugosita ó si se 
 %      come a Remeditos la vaquita. Además, pumba es picarón de por sí. 
+
 %?-picarones(L). 
 %L = [pumba, timon, simba] 
 
@@ -81,4 +100,121 @@ comioRemeditos(Personaje) :-
     comio(Personaje, vaquitaSanAntonio(remeditos,_)).
 
 % 2)
+
+% 2)a) Se quiere saber cuánto engorda un personaje (sabiendo que engorda
+%      una cantidad igual a la suma de los pesos de todos los bichos en 
+%      su menú). Los bichos no engordan. 
+
+%?-cuantoEngorda(Personaje, Peso). 
+%Personaje= pumba 
+%Peso = 83; 
+%Personaje= timon 
+%Peso = 17; 
+%Personaje= simba 
+%Peso = 10
+
+cuantoEngorda(Personaje,Peso) :-
+    personajesLivianos(Personaje),
+    findall(Bichos,comio(Personaje,Bichos),ListaBichos),
+    sumaPesos(ListaBichos,Peso).
+personajesLivianos(Personaje) :- peso(Personaje,50).
+personajesLivianos(Personaje) :- peso(Personaje,100).
+personajesLivianos(Personaje) :- peso(Personaje,200).
+
+sumaPesos([],0).
+sumaPesos([Bicho|Resto], Suma) :-
+    pesoBicho(Bicho,Peso),
+    sumaPesos(Resto,SumaParcial),
+    Suma is Peso + SumaParcial.
+
+pesoBicho(vaquitaSanAntonio(_, Peso), Peso).
+pesoBicho(hormiga(_),Peso) :-
+    pesoHormiga(Peso).
+pesoBicho(cucaracha(_,_,Peso),Peso).
+
+% 2)b) Pero como indica la ley de la selva, cuando un personaje persigue 
+%      a otro, se lo termina comiendo, y por lo tanto también engorda. 
+%      Realizar una nueva versión del predicado cuantoEngorda. 
+
+%?-cuantoEngorda(scar,Peso). 
+%Peso = 150 
+%(es la suma de lo que pesan pumba y timon) 
+
+%?-cuantoEngorda(shenzi,Peso). 
+%Peso = 502 
+%(es la suma del peso de scar y simba, más 2 que pesa la hormiga) 
+
+cuantoEngorda2(Personaje,PesoComida) :-
+    personajes2(Personaje),
+    bagof(Comida,comida2(Personaje,Comida),ListaComida), 
+    sumaPesos2(ListaComida,PesoComida).
+personajes2(Personaje):- peso(Personaje,_).
+comida2(Personaje,Presa) :-
+    persigue(Personaje,Presa).
+comida2(Personaje,Presa) :-
+    comio(Personaje,Presa).
+
+sumaPesos2([],0).
+sumaPesos2([Comida|Resto], Suma) :-
+    pesoComida2(Comida,Peso),
+    sumaPesos2(Resto,SumaParcial),
+    Suma is Peso + SumaParcial.
+
+pesoComida2(Personaje,Peso) :-
+    peso(Personaje,Peso).
+pesoComida2(vaquitaSanAntonio(_, Peso), Peso).
+pesoComida2(hormiga(_),Peso) :-
+    pesoHormiga(Peso).
+pesoComida2(cucaracha(_,_,Peso),Peso).
+
+% 2)c) Ahora se complica el asunto, porque en realidad cada animal antes
+%      de comerse a sus víctimas espera a que éstas se alimenten. De esta
+%      manera, lo que engorda un animal no es sólo el peso original de sus
+%      víctimas, sino también hay que tener en cuenta lo que éstas comieron
+%      y por lo tanto engordaron. Hacer una última versión del predicado. 
+
+%?-cuantoEngorda(scar,Peso). 
+%Peso = 250 
+
+%(150, que era la suma de lo que pesan pumba y timon, más 83 que se come 
+%pumba y 17 que come timon ) 
+
+%?-cuantoEngorda(shenzi,Peso). 
+%Peso = 762 
+
+%(502 era la suma del peso de scar y simba, más 2 de la hormiga. A eso se 
+%le suman los 250 de todo lo que engorda scar y 10 que engorda simba) 
+
+cuantoEngorda3(Personaje,PesoComida) :-
+    personajes3(Personaje),
+    bagof(Comida,comida3(Personaje,Comida),ListaComida), 
+    sumaPesos3(ListaComida,PesoComida).
+personajes3(Personaje):- peso(Personaje,_).
+comida3(Personaje,Presa) :-
+    persigue(Personaje,Presa).
+comida3(Personaje,Presa) :-
+    comio(Personaje,Presa).
+
+sumaPesos3([],0).
+sumaPesos3([Comida|Resto], Suma) :-
+    pesoComida3(Comida,Peso),
+    sumaPesos3(Resto,SumaParcial),
+    Suma is Peso + SumaParcial.
+
+pesoComida3(Personaje,Peso) :-
+    peso(Personaje,PesoPresa),
+    cuantoEngorda3(Personaje,PesoComidaDeComida),
+    Peso is PesoPresa + PesoComidaDeComida.
+pesoComida3(vaquitaSanAntonio(_, Peso), Peso).
+pesoComida3(hormiga(_),Peso) :-
+    pesoHormiga(Peso).
+pesoComida3(cucaracha(_,_,Peso),Peso).
+
+% 3) Para acelerar el plato de comida… Se quiere saber todas las posibles 
+%    combinaciones posibles de comidas que puede tener un personaje dado.
+%    Se sabe que la comida no es solo lo que comió si no también los 
+%    animales que persigue.
+
+%combinaComidas(Personaje, ListaComidas)
+
 
